9d5b1b57de850b6794d9f60c2170c57da3bbd6ff
thNel <nel.alexen@gmail.com>
2025-07-24 15:52:45 +0400

    Revert "fix: declare virtual pages module"
    This reverts commit dcbd1bfd308eec0bffb9f06c311430e9ebda67d5.

--- apps/frontend/src/vite-env.d.ts
+++ apps/frontend/src/vite-env.d.ts
@@ -1,8 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client-react" />
-
-declare module 'virtual:generated-pages-react' {
-  import type { RouteObject } from 'react-router';
-  const routes: RouteObject[];
-  export default routes;
-}
